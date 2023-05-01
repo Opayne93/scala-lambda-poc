64ef69f1cb554b9609a880cca97a01d2e3b60f95
Oliver Payne <oliver.payne@lifeway.com>
2023-05-01 07:05:09 -0500

    remove comment

--- infrastructure/src/main/scala/com/lifeway/aom/AppStack.scala
+++ infrastructure/src/main/scala/com/lifeway/aom/AppStack.scala
@@ -45,31 +45,6 @@ class AppStack(
 
   val version = scalaPocHandler.getCurrentVersion()
 
-// private Version createUnicornLambdaFunction() {
-//         var lambda =  Function.Builder.create(this, "UnicornStoreSpringFunction")
-//                 .runtime(Runtime.JAVA_11)
-//                 .functionName("unicorn-store-spring")
-//                 .memorySize(2048)
-//                 .timeout(Duration.seconds(29))
-//                 .code(Code.fromAsset("../../software/unicorn-store-spring/target/store-spring-1.0.0-aws.jar"))
-//                 .handler("org.springframework.cloud.function.adapter.aws.FunctionInvoker::handleRequest")
-//                 .vpc(infrastructureStack.getVpc())
-//                 .securityGroups(List.of(infrastructureStack.getApplicationSecurityGroup()))
-//                 .environment(Map.of(
-//                     "SPRING_DATASOURCE_PASSWORD", infrastructureStack.getDatabaseSecretString(),
-//                     "SPRING_DATASOURCE_URL", infrastructureStack.getDatabaseJDBCConnectionString(),
-//                     "SPRING_DATASOURCE_HIKARI_maximumPoolSize", "1",
-//                     "AWS_SERVERLESS_JAVA_CONTAINER_INIT_GRACE_TIME", "500"
-//                     ,"JAVA_TOOL_OPTIONS", "-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
-//                 ))
-//                 .build();
-//
-//         // Enable Snapstart
-//         CfnFunction cfnFunction = (CfnFunction) lambda.getNode().getDefaultChild();
-//         cfnFunction.addPropertyOverride("SnapStart", Map.of("ApplyOn", "PublishedVersions"));
-//         // Return version
-//         return lambda.getCurrentVersion();
-//     }
 
   Tags.of(this).add("Department", "digital-experience")
   Tags.of(this).add("Owner", "aom")
